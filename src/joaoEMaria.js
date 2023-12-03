5005ffd9b949d3625abcebcbdb5e84f57fd9a943
DanrleyGD <danrley.dias@estudante.ufjf.br>
2023-12-03 18:19:27 -0300

    cria get e set

---
+++ src/joaoEMaria.js
@@ -0,0 +1,15 @@
+let joao = 1;
+let maria = 2;
+
+function getJoao() {
+    return joao;
+  }
+  function getMaria() {
+    return maria;
+  }
+  function setMatia(m) {
+    maria = m;
+  }
+  function setJoao(m) {
+      joao = m;
+  }
