98a20ed5653432aa4ca7945579b79702a04466cb
DanrleyGD <danrley.dias@estudante.ufjf.br>
2023-12-03 18:25:20 -0300

    cria funcoes deMariaParaJoao e deJoaoParaMaria

--- src/joaoEMaria.js
+++ src/joaoEMaria.js
@@ -1,15 +1,23 @@
 let joao = 1;
 let maria = 2;
 
+function deJoaoParaMaria() {
+  maria += joao;
+  joao = 0;
+}
+function deMariaParaJoao() {
+  joao += maria;
+  maria = 0;
+}
 function getJoao() {
-    return joao;
-  }
-  function getMaria() {
-    return maria;
-  }
-  function setMatia(m) {
-    maria = m;
-  }
-  function setJoao(m) {
-      joao = m;
-  }
+  return joao;
+}
+function getMaria() {
+  return maria;
+}
+function setMatia(m) {
+  maria = m;
+}
+function setJoao(m) {
+  joao = m;
+}
